ee3dffdf7679cc6a5a8a060a732fdd51e4373d3f
Charlie Vieth <cviethjr@pivotal.io>
2017-04-13 14:21:01 -0400

    wip update run_unit_windows powershell script
    Signed-off-by: Brandon Shroyer <bshroyer@pivotal.io>

--- scripts/ci/run_unit_windows.ps1
+++ scripts/ci/run_unit_windows.ps1
@@ -1,19 +1,12 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-function absolute_path() {
-  cd $1
-  if ($?) {
-    pwd
-  }
-}
-
 cd diego-release/
 
 $env:GOPATH=($pwd).path
 $env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
-Write-Host "Gopath is $GOPATH"
-Write-Host "PATH is $PATH"
+# Write-Host "Gopath is " + $env:GOPATH
+# Write-Host "PATH is " + $env:PATH
 
 go install github.com/apcera/gnatsd
 # go install github.com/coreos/etcd
@@ -26,37 +19,16 @@ if ($LastExitCode -ne 0) {
   Write-Host "Ginkgo successfully installed"
 }
 
-# scripts_path=$(absolute_path `dirname $0`)
-
-# Write-Host "Setting environment variables..."
-# $env:DIEGO_RELEASE_DIR=$(absolute_path $env:scripts_path/..)
-# $env:CF_RELEASE_DIR=$(absolute_path $env:DIEGO_RELEASE_DIR/../cf-release)
-
-# $env:CF_MANIFESTS_DIR=$env:CF_RELEASE_DIR + "/bosh-lite/deployments"
-# $env:DIEGO_MANIFESTS_DIR=$env:DIEGO_RELEASE_DIR + "/bosh-lite/deployments"
-
-# Write-Host $env:DIEGO_RELEASE_DIR=$env:DIEGO_RELEASE_DIR
-# Write-Host $env:CF_RELEASE_DIR=$env:CF_RELEASE_DIR
-# Write-Host $env:CF_MANIFESTS_DIR=$env:CF_MANIFESTS_DIR
-# Write-Host $env:DIEGO_MANIFESTS_DIR=$env:DIEGO_MANIFESTS_DIR
-
-
 Write-Host "Running store-dependent test suites against a MySQL database..."
 $env:DB_UNITS="./bbs/db/sqldb"
-$env:SQL_FLAVOR=mysql
+$env:SQL_FLAVOR="mysql"
 
 cd src/code.cloudfoundry.org/
 
-ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race rep/cmd/rep rep/generator/internal route-emitter/cmd/route-emitter bbs/db/sqldb
-    # rep/cmd/rep \
-    # rep/generator/internal \
-    # route-emitter/cmd/route-emitter \
-    # ./bbs/db/sqldb
-
-
+ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race rep/cmd/rep
+# ginkgo -r -keepGoing -p -trace -randomizeAllSpecs -progress --race rep/cmd/rep rep/generator/internal route-emitter/cmd/route-emitter bbs/db/sqldb
 
 # $scripts_path/run-unit-tests-no-backing-store
-# let ERROR_CODE+=$?
 
 if ($LastExitCode -ne 0) {
     Write-Host "Diego unit tests failed"
